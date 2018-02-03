7cbc96ce6694dd0af0ba8b914410548872b44ae9
svcosta <sergio.@hotmail.com>
2018-02-03 18:03:42 +0000

    Aula 4

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -3,17 +3,20 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { FotoModule } from "./foto/foto.module";
-
 // importou o módulo que já possui um provider configurado
 import { HttpModule } from "@angular/http";
 
 // HttpModule adicionadno no array de imports!
+import { PainelModule } from './painel/painel.module';
+
+import { CadastroComponent }   from './cadastro/cadastro.component'; // importou
+import { ListagemComponent }   from './listagem/listagem.component'; // importou
+import { routing } from './app.routes';
+
+
 @NgModule({
-  imports:      [ BrowserModule, FotoModule, HttpModule],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FotoModule, HttpModule, PainelModule, routing ],
+  declarations: [ AppComponent,CadastroComponent, ListagemComponent],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule {  
-
-
-}
+export class AppModule { }
